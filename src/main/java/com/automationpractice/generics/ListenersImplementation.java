bfb886301e02b6e392830b13f6b77e71fbd8f3bd
arkaqa <67999962+arkaqa@users.noreply.github.com>
2020-08-25 12:28:36 +0530

    First Commit

---
+++ src/main/java/com/automationpractice/generics/ListenersImplementation.java
@@ -0,0 +1,59 @@
+package com.automationpractice.generics;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenersImplementation extends TestBase implements ITestListener {
+
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult results) {
+		String TestCaseName = results.getName();
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File srcfile = ts.getScreenshotAs(OutputType.FILE);
+		File destfile= new File("./Screenshots/"+TestCaseName+".png");
+		try {
+			FileUtils.copyFile(srcfile, destfile);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/main/java/com/automationpractice/generics/TestBase.java
@@ -0,0 +1,5 @@
+package com.automationpractice.generics;
+
+public class TestBase {
+
+}
